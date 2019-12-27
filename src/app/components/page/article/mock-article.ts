65cb235265958b4bbc8559d1516ba92142dc7566
Tanya Chudna <tanya.papushoi@gmail.com>
2019-12-27 12:52:39 +0200

    Add images

--- src/app/components/page/article/mock-article.ts
+++ src/app/components/page/article/mock-article.ts
@@ -18,5 +18,17 @@ export const ARTICLES: Article[] = [
     description: 'Whether they\'re an angel or sheep number 3, they\'ll steal the show in our nativity costumes',
     image: 'casting_call.jpeg',
     descriptionText: 'Fairy £10 | Shepherd £10 | Star £8 | Angel £10 | Sheep £10'
+  },
+  {
+    title: 'Girls\' shirts',
+    description: 'Our comfy cotton blend shirts are brilliantly bright and made to stay that way',
+    image: 'girls_clothing.jpeg',
+    descriptionText: 'Slim fit 2 pack from £3.50 | Revere collar 2 pack from £3 | Pin tuck 2 pack from £5'
+  },
+  {
+    title: 'Boys\' shirts',
+    description: 'They\'ll be looking smart and feeling great in whichever fit suits them best',
+    image: 'boys_clothing.jpeg',
+    descriptionText: 'Regular 2 pack from £3.50 | Slim fit 2 pack from £3.50 | Stretch 2 pack from £5'
   }
 ];
