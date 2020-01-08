e62b76581bff9483bdc7e4c80c9f7f922423f657
Tanya Chudna <tanya.papushoi@gmail.com>
2020-01-09 00:18:02 +0200

    Fix code in blog component and modify main style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BlogComponent} from './components/blog/blog.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import {NgbCarouselModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BlogComponent
+    BlogComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
