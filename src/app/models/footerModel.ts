d24f7dcb6ae36b6794aa4e888d4a1c126e12762a
Tanya Chudna <tanya.papushoi@gmail.com>
2020-01-24 13:08:10 +0200

    Add footer model

---
+++ src/app/models/footerModel.ts
@@ -0,0 +1,28 @@
+export class FooterModel {
+  public plates = [
+    {
+      title: 'Lorem ipsum dolor sit',
+      // tslint:disable-next-line:max-line-length
+      description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita sapiente sint, nulla, nihil repudiandae commodi voluptatibus corrupti animi sequi aliquid magnam debitis, maxime quam recusandae harum esse fugiat. Itaque, culpa?'
+    }, {
+      title: 'Lorem ipsum dolor',
+      // tslint:disable-next-line:max-line-length
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio deserunt fuga perferendis modi earum commodi aperiam temporibus quod nulla nesciunt aliquid debitis ullam omnis quos ipsam, aspernatur id excepturi hic.'
+    }
+  ];
+
+  public icons = [{
+    icon: 'facebook'
+  }, {
+    icon: 'twitter'
+  }, {
+    icon: 'google-plus'
+  }, {
+    icon: 'linkedin'
+  }, {
+    icon: 'instagram'
+  }, {
+    icon: 'pinterest'
+  }
+  ];
+}
