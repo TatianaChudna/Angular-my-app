f12bcf8446b8ace46b8c89f84bc0e30f9bf98236
Tanya Chudna <tanya.papushoi@gmail.com>
2020-01-24 13:27:45 +0200

    Fix footer component

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {FooterModel} from '../../models/footer.model';
-import {IconModel} from '../../models/icon.model';
+import {FooterModel} from '../../models/footerModel';
+import {FooterDescriptionModel} from '../../models/footer-description.model';
 
 @Component({
   selector: 'app-footer',
@@ -8,25 +8,10 @@ import {IconModel} from '../../models/icon.model';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-  public plates: FooterModel[];
-  public icons: IconModel[];
+  public descriptionModel: FooterDescriptionModel = new FooterDescriptionModel();
+  public model: FooterModel = new FooterModel();
 
   constructor() {
-    this.plates = [
-      // tslint:disable-next-line:max-line-length
-      new FooterModel('Lorem ipsum dolor sit', 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita sapiente sint, nulla, nihil repudiandae commodi voluptatibus corrupti animi sequi aliquid magnam debitis, maxime quam recusandae harum esse fugiat. Itaque, culpa?'),
-      // tslint:disable-next-line:max-line-length
-      new FooterModel('Lorem ipsum dolor', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio deserunt fuga perferendis modi earum commodi aperiam temporibus quod nulla nesciunt aliquid debitis ullam omnis quos ipsam, aspernatur id excepturi hic.')
-    ];
-
-    this.icons = [
-      new IconModel('facebook'),
-      new IconModel('twitter'),
-      new IconModel('google-plus'),
-      new IconModel('linkedin'),
-      new IconModel('instagram'),
-      new IconModel('pinterest')
-    ];
   }
 
   ngOnInit() {
