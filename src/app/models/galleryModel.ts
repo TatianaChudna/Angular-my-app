d0d82e262d5badacb586a5a280cb96ee8f4e0291
Tanya Chudna <tanya.papushoi@gmail.com>
2020-01-24 13:08:54 +0200

    Add gallery model

---
+++ src/app/models/galleryModel.ts
@@ -0,0 +1,11 @@
+export class GalleryModel {
+  public images = [
+    {
+      image: 'party_girl.jpeg'
+    }, {
+      image: 'christmas_cuties.jpeg'
+    }, {
+      image: 'casting_call.jpeg'
+    }
+  ];
+}
