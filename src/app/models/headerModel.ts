93be12f12510f485b38ae281f2ad3c98866885c3
Tanya Chudna <tanya.papushoi@gmail.com>
2020-01-24 13:09:10 +0200

    Add header model

---
+++ src/app/models/headerModel.ts
@@ -0,0 +1,23 @@
+export class HeaderModel {
+  public lists = [
+    {
+      name: 'Home',
+      route: '/home'
+    }, {
+      name: 'Gallery',
+      route: '/gallery'
+    }, {
+      name: 'List & table',
+      route: '/table'
+    }, {
+      name: 'Blog',
+      route: '/blog'
+    }, {
+      name: 'Login',
+      route: '/login'
+    }, {
+      name: 'Registration',
+      route: '/registration'
+    }
+  ];
+}
