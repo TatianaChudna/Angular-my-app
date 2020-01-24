d7c24c247e956165da4222666622dc2e639c5fe6
Tanya Chudna <tanya.papushoi@gmail.com>
2020-01-24 13:09:37 +0200

    Add list model

---
+++ src/app/models/listModel.ts
@@ -0,0 +1,34 @@
+export class ListModel {
+  public listItem = [
+    {
+      name: '- Cras justo odio'
+    }, {
+      name: '- Dapibus ac facilisis in'
+    }, {
+      name: '- Morbi leo risus'
+    }, {
+      name: '- Porta ac consectetur ac'
+    }, {
+      name: '- Vestibulum at eros'
+    }
+  ];
+
+  public elements: any = [
+    {
+      id: 1,
+      first: 'Mark',
+      last: 'Otto',
+      handle: '@google'
+    }, {
+      id: 2,
+      first: 'Jacob',
+      last: 'Thornton',
+      handle: '@facebook'
+    }, {
+      id: 3,
+      first: 'Larry',
+      last: 'Doe',
+      handle: '@twitter'
+    },
+  ];
+}
