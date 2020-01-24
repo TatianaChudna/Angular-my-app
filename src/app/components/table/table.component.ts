66147f9a4beccc6040b07a372db34065a0053958
Tanya Chudna <tanya.papushoi@gmail.com>
2020-01-24 13:24:17 +0200

    Fix table component

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {ListModel} from '../../models/list.model';
+import {ListModel} from '../../models/listModel';
+import {ListDescriptionModel} from '../../models/list-description.model';
 
 @Component({
   selector: 'app-table',
@@ -7,25 +8,11 @@ import {ListModel} from '../../models/list.model';
   styleUrls: ['./table.component.scss']
 })
 export class TableComponent implements OnInit {
-
-  public elements: any = [
-    {id: 1, first: 'Mark', last: 'Otto', handle: '@google'},
-    {id: 2, first: 'Jacob', last: 'Thornton', handle: '@facebook'},
-    {id: 3, first: 'Larry', last: 'Doe', handle: '@twitter'},
-  ];
-
+  public descriptionList: ListDescriptionModel = new ListDescriptionModel();
   public headElements = ['ID', 'First', 'Last', 'Handle'];
-
-  public listItem: ListModel[];
+  public item: ListModel = new ListModel();
 
   constructor() {
-    this.listItem = [
-      new ListModel('- Cras justo odio'),
-      new ListModel('- Dapibus ac facilisis in'),
-      new ListModel('- Morbi leo risus'),
-      new ListModel('- Porta ac consectetur ac'),
-      new ListModel('- Vestibulum at eros'),
-    ];
   }
 
   ngOnInit() {
